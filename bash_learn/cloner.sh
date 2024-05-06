9fcfdd94a5ce73d3526aff06e3759432a628a168
juto-shogan <somtochukwu.mbonu@binghamuni.edu.ng>
2024-05-06 19:16:46 +0100

    Modified

--- bash_learn/cloner.sh
+++ bash_learn/cloner.sh
@@ -2,9 +2,25 @@
 
 cd /home/bruut/Desktop/clones
 
-echo "repo to clone"
-read to_clone 
-git clone $to_clone
+
+while true; do 
+  echo "repo to clone"
+  read to_clone 
+
+  if [ "$to_clone"]; then
+  break
+  fi
+
+  git clone $to_clone
+
+  read -p "Clone another repository (y/n)? " continue_cloning
+
+  
+  if [[ "$continue_cloning" != "y" ]]; then
+    break
+  fi
+done
+
 
 echo "done"
 
