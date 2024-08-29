32696355828e4c311d37e24c21cf2b55ce95b0c5
beckamjoseph <seyij556@gmail.com>
2024-08-29 20:41:20 +0100

    work'

---
+++ bash_learn/Seyi/variable.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+FIRST_NAME=Oluwaseyi
+LAST_NAME=Joseph
+echo hello $FIRST_NAME $LAST_NAME
+
+
+## to collect input from user
+echo what is your first name?
+read $FIRST_NAME
+echo what is your last name?
+read $LAST_NAME
+echo hello $FIRST_NAME $LAST_NAME
